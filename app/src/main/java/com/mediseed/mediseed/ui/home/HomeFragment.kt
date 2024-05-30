0413b1fbd85b118b6fec83e67c37f3bb76a28621
kang <guswjd9994@duksung.ac.kr>
2024-05-30 16:45:19 +0900

    test: Modal Bottom Sheet

--- app/src/main/java/com/mediseed/mediseed/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mediseed/mediseed/ui/home/HomeFragment.kt
@@ -5,19 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.mediseed.mediseed.R
+import com.mediseed.mediseed.BottomSheetFragment
+import com.mediseed.mediseed.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.showBottomSheetButton.setOnClickListener {
+            val bottomSheet = BottomSheetFragment()
+            bottomSheet.show(childFragmentManager, bottomSheet.tag)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance() = HomeFragment()
     }
 }
